5f7e02bcca37ca7784015560b005ba2d236808f4
MaximeD <staphir@gmail.com>
2018-11-24 18:21:41 +0100

    tout casté en set (donc modification @proprety et  __str__) + __eq__ + access

--- Automaton.py
+++ Automaton.py
@@ -7,17 +7,17 @@ class Automaton(object):
     def __init__(self, *args):
         if len(args) == 0:
             self._etats = {0}
-            self._alphabet = ""
-            self._transitions = []
-            self._initiaux = [0]
-            self._terminaux = [0]
+            self._alphabet = set()
+            self._transitions = set()
+            self._initiaux = {0}
+            self._terminaux = {0}
         elif len(args) == 1 and args[0].split(".")[1] == "aut":
             file = BasicReader(args[0])
-            self._etats = file.etats
-            self._alphabet = file.alphabet
-            self._transitions = file.transitions
-            self._initiaux = file.initiaux
-            self._terminaux = file.terminaux
+            self._etats = self.transform_to_set(file.etats)
+            self._alphabet = self.transform_to_set(file.alphabet)
+            self._transitions = self.transform_to_set(file.transitions)
+            self._initiaux = self.transform_to_set(file.initiaux)
+            self._terminaux = self.transform_to_set(file.terminaux)
         elif len(args) == 5:
             self._etats = args[0]
             self._alphabet = args[1]
@@ -25,14 +25,14 @@ class Automaton(object):
             self._initiaux = args[3]
             self._terminaux = args[4]
 
-            self._etats = self.verif_etats()
+            self._etats = self.transform_to_set(self.verif_etats())
             # N'accepte que les entiers
-            self._alphabet = self.verif_alphabet()
+            self._alphabet = self.transform_to_set(self.verif_alphabet())
             # N'accepte que les caractères alphanumériques, donc l'alphabet et les chiffres de 0 à 9
-            self._transitions = self.verif_trans()
+            self._transitions = self.transform_to_set(self.verif_trans())
             # N'accepte que les triplets de type int-str-int
-            self._initiaux = self.verif_initiaux()
-            self._terminaux = self.verif_terminaux()
+            self._initiaux = self.transform_to_set(self.verif_initiaux())
+            self._terminaux = self.transform_to_set(self.verif_terminaux())
         else:
             print("Automate non conforme vous avez trois choix :\n"
                   "- Automate() -> automate reconnaissant le langage vide\n"
@@ -61,15 +61,15 @@ class Automaton(object):
     @property
     def automata(self): return (list(self._etats),list(self._alphabet),list(self._transitions),list(self._initiaux),list(self._terminaux))
     @property
-    def Q(self): return sorted(self._etats)
+    def Q(self): return sorted(list(self._etats))
     @property
-    def Sigma(self): return sorted(self._alphabet)
+    def Sigma(self): return sorted(list(self._alphabet))
     @property
-    def Delta(self): return sorted(self._transitions)
+    def Delta(self): return sorted(list(self._transitions))
     @property
-    def S(self): return sorted(self._initiaux)
+    def S(self): return sorted(list(self._initiaux))
     @property
-    def T(self): return sorted(self._terminaux)
+    def T(self): return sorted(list(self._terminaux))
 
     # ------------------------------------------------------------------------------
     def __repr__(self):
@@ -97,9 +97,9 @@ class Automaton(object):
         text = "Etats :\n "
         for e in self._etats: text += str(e) + " "
         text += "\nAlphabet :\n "
-        for a in self._alphabet: text += a + " "
+        for a in sorted(list(self._alphabet)): text += a + " "
         text += "\nTransitions :\n"
-        for triplet in reversed(self._transitions):
+        for triplet in sorted(list(self._transitions)):
             for c in triplet: text += " " + str(c) + " "
             text += "\n"
         text += "Etats initiaux :\n "
@@ -110,10 +110,23 @@ class Automaton(object):
         return text
 
     # ------------------------------------------------------------------------------
+
+    def __eq__(self, other):
+        assert isinstance(other,Automaton), "comparaison possible uniquement entre deux automates"
+        return self._alphabet == other.Sigma
+
         #On veut que la fonction permette de choisir un argument pour aller voir ses valeurs. 
         #On prend les informations renvoyées par __repr__ lorsqu'elles ont été converties en str. 
         #Puis on les classe en fonction des états, des transitions... Et on les affiche avec les 
         #titres correspondants.
+
+    def transform_to_set(self,var):
+        s = set()
+        for i in var:
+            s.add(i)
+        return s
+
+
     def verif_etats(self):
         new_etats = set()
         for etat in self._etats:
@@ -175,7 +188,18 @@ class Automaton(object):
         # On a aussi l etat puits qui correspond au traitement errone d un etat
 
     def access(self):
-        pass
+        e_access = self._initiaux
+        e_second = e_access
+        end = False
+        while end == False:
+            for triplet in self._transitions:
+                if triplet[0] in e_access:
+                    e_second.add(triplet[2])
+            if e_access == e_second:
+                end = True
+            else:
+                e_access.union(e_second)
+        return Automaton(e_access,self._alphabet,self._transitions,self._initiaux,self._terminaux)
         # pour chaque état initial regarder s'il existe un triplet qui a pour début l'etat initial pour chaque états non initiaux
 
 # ==============================================================================
@@ -184,9 +208,10 @@ class Automaton(object):
 
 # ==============================================================================
 if __name__ == "__main__":
-    # a = Automaton(range(4), "bca", [(0, 'a', 0), (0, 'b', 1), (2, 'cc', 3)], [0,2], [1])
-    a = Automaton("automata/automata_0.aut")
+    a = Automaton(range(4), "bca", [(0, 'a', 0), (0, 'b', 1), (2, 'cc', 0)], [0,2], [1])
+    b = Automaton("automata/automata_1.aut")
     # a = Automaton()
     # print(a)
-    print(repr(a.automata))
+    # print(repr(a.automata))
+    print(a==8)
     # ==============================================================================
