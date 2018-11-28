1aa741a2f912c738558fafc0e7d69b7fdb5e2e2a
MaximeD <staphir@gmail.com>
2018-11-28 12:10:56 +0100

    Dernière étape déterminisation à finir...

--- Automaton.py
+++ Automaton.py
@@ -11,13 +11,20 @@ class Automaton(object):
             self._transitions = set()
             self._initiaux = {0}
             self._terminaux = {0}
-        elif len(args) == 1 and args[0].split(".")[1] == "aut":
-            file = BasicReader(args[0])
-            self._etats = self.transform_to_set(file.etats)
-            self._alphabet = self.transform_to_set(file.alphabet)
-            self._transitions = self.transform_to_set(file.transitions)
-            self._initiaux = self.transform_to_set(file.initiaux)
-            self._terminaux = self.transform_to_set(file.terminaux)
+        elif len(args) == 1:
+            if not BasicReader(args[0]) == []:
+                file = BasicReader(args[0])
+                self._etats = self._transform_to_set(file.etats)
+                self._alphabet = self._transform_to_set(file.alphabet)
+                self._transitions = self._transform_to_set(file.transitions)
+                self._initiaux = self._transform_to_set(file.initiaux)
+                self._terminaux = self._transform_to_set(file.terminaux)
+            else:
+                self._etats = {0}
+                self._alphabet = set()
+                self._transitions = set()
+                self._initiaux = {0}
+                self._terminaux = {0}
         elif len(args) == 5:
             self._etats = args[0]
             self._alphabet = args[1]
@@ -25,19 +32,20 @@ class Automaton(object):
             self._initiaux = args[3]
             self._terminaux = args[4]
 
-            self._etats = self.transform_to_set(self.verif_etats())
+            self._etats = self._transform_to_set(self._verif_etats())
             # N'accepte que les entiers
-            self._alphabet = self.transform_to_set(self.verif_alphabet())
+            self._alphabet = self._transform_to_set(self._verif_alphabet())
             # N'accepte que les caractères alphanumériques, donc l'alphabet et les chiffres de 0 à 9
-            self._transitions = self.transform_to_set(self.verif_trans())
+            self._transitions = self._transform_to_set(self._verif_trans())
             # N'accepte que les triplets de type int-str-int
-            self._initiaux = self.transform_to_set(self.verif_initiaux())
-            self._terminaux = self.transform_to_set(self.verif_terminaux())
+            self._initiaux = self._transform_to_set(self._verif_initiaux())
+            self._terminaux = self._transform_to_set(self._verif_terminaux())
         else:
             print("Automate non conforme vous avez trois choix :\n"
                   "- Automate() -> automate reconnaissant le langage vide\n"
                   "- Automate(file.aut) -> automate créé à partir d'un fichier .aut\n"
                   "- Automate(Q,Sigma,Delta,initial_states,accepting_states)")
+            return
 
         self._afd = True
         self._afdc = False
@@ -120,29 +128,29 @@ class Automaton(object):
         #Puis on les classe en fonction des états, des transitions... Et on les affiche avec les 
         #titres correspondants.
 
-    def transform_to_set(self,var):
+    def _transform_to_set(self,var):
         s = set()
         for i in var:
             s.add(i)
         return s
 
 
-    def verif_etats(self):
+    def _verif_etats(self):
         new_etats = set()
         for etat in self._etats:
             if isinstance(etat, int):
                 new_etats.add(etat)
         return new_etats
 
-    def verif_alphabet(self):
+    def _verif_alphabet(self):
         # boucle chaque caractère à faire pour garder que les bon caractères
         new_alphabet = ''
         for lettre in self._alphabet:
-            if 47 < ord(str(lettre)) < 58 or 64 < ord(str(lettre)) < 91 or 96 < ord(str(lettre)) < 123:
+            if 47 < ord(str(lettre)) < 58 or 64 < ord(str(lettre)) < 91 or 96 < ord(str(lettre)) < 123 or not str(lettre):
                 new_alphabet += lettre
         return new_alphabet
 
-    def verif_trans(self):
+    def _verif_trans(self):
         new_transitions = []
         for triplet in self._transitions:
             if type(triplet[0]) == int and triplet[0] in self._etats and type(triplet[2]) == int and triplet[2] in self._etats:
@@ -160,7 +168,7 @@ class Automaton(object):
             return new_transitions
         # besoin de test int-str-int dans une boucle car tableau de transitions
 
-    def verif_initiaux(self):
+    def _verif_initiaux(self):
         new_initiaux = []
         for etat_init in self._initiaux:
             if isinstance(etat_init, int) and etat_init in self._etats:
@@ -170,7 +178,7 @@ class Automaton(object):
         else:
             return new_initiaux
 
-    def verif_terminaux(self):
+    def _verif_terminaux(self):
         new_terminaux = []
         for etat_term in self._terminaux:
             if isinstance(etat_term, int) and etat_term in self._etats:
@@ -189,7 +197,7 @@ class Automaton(object):
 
     def access(self):
         e_access = self._initiaux.copy()
-        e_second = e_access
+        e_second = e_access.copy()
         end = False
         while end == False:
             for triplet in self._transitions:
@@ -198,7 +206,7 @@ class Automaton(object):
             if e_access == e_second:
                 end = True
             else:
-                e_access.union(e_second)
+                e_access.update(e_second)
         return Automaton(e_access,self._alphabet,self._transitions,self._initiaux,self._terminaux)
 
 
@@ -209,12 +217,12 @@ class Automaton(object):
         # 1-un seul état initial
         e_new.add("s")
         for init in self._initiaux:
-            t_new.add(('s','',str(init)))
+            t_new.add(('s','',init))
 
         # 2-un seul état final
         e_new.add("f")
         for term in self._terminaux:
-            t_new.add((str(term),'','f'))
+            t_new.add((term,'','f'))
 
         # 3-éliminer les liaisons suppérieurs à 1
         for triplet in self._transitions:
@@ -231,9 +239,53 @@ class Automaton(object):
                     t_new.add((str(triplet[0])+"q"+str(i-1),triplet[1][i-1],str(triplet[0])+"q"+str(i)))
                 t_new.add((str(triplet[0])+"q"+str(long-1),triplet[1][long-1],triplet[2]))
 
-        # 4-tableau déterminisation
+        # 4-nouvel état sans épsilones
+        e_epsi = {}
+        continuer = True
+        for etat in e_new:
+            e_tmp = {etat}
+            while continuer == True:
+                len_past = len(e_tmp)
+                continuer = False
+                for triplet in t_new:
+                    if triplet[0] in e_tmp and triplet[1] == '':
+                        e_tmp.add(triplet[2])
+                if len(e_tmp)>len_past:
+                    continuer = True
+            e_epsi[etat] = e_tmp
+            continuer = True
 
-
+        # 5-tableau de fusion
+        e_deter = {0:e_epsi['s']}
+        dict_trans = {}
+        end = False
+        nbe = 0
+        e_t = 0
+        while end == False:
+            end = True
+            while e_t in e_deter.keys():
+                e_d = e_deter[e_t]
+                for lettre in self._alphabet:
+                    s_tmp = set()
+                    for t in e_d:
+                        for triplet in self._transitions:
+                            if triplet[0] == t and triplet[1] == lettre:
+                                s_tmp.add(triplet[2])
+                        e_equi = set()
+                        for e_acc in s_tmp:
+                            e_equi.update(e_epsi[e_acc])
+                        if not e_equi in e_deter.values() and e_equi :
+                            nbe += 1
+                            e_deter[nbe] = e_equi
+                            dict_trans[nbe] = set()
+                            end = False
+                        elif e_equi:
+                            for key in e_deter.keys():
+                                if e_deter[key] == e_equi:
+                                    dict_trans[nbe].update({(e_t,lettre,key)})
+                e_t += 1
+        print(e_deter)
+        print(dict_trans)
         self._afn = False
         self._afd = True
 # ==============================================================================
@@ -242,10 +294,10 @@ class Automaton(object):
 
 # ==============================================================================
 if __name__ == "__main__":
-    # a = Automaton(range(4), "bca", [(0, 'a', 0), (0, 'b', 1), (2, 'cabc', 0)], [0,2], [1])
-    b = Automaton("automata/automata_1.aut")
+    # a = Automaton(range(4), "bca", [(0, 'a', 0), (0, '', 1), (2, 'cabc', 0), (3,'b',2)], [0,2], [1])
+    b = Automaton("automata/automata_cours.aut")
     # a = Automaton()
-    # print(a)
     # print(repr(a.automata))
-    print(b.automata)
+    # print(b.automata)
+    b.deterministe()
     # ==============================================================================
